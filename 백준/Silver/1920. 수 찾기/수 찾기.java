c58491d0bc9daea7fc0a09da180a0b20d168a43c
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-05-03 09:46:38 +0900

    [Silver IV] Title: 수 찾기, Time: 1320 ms, Memory: 88208 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+public static void main(String[] args) throws NumberFormatException, IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int N = Integer.parseInt(br.readLine());
+	HashMap<String,Long> map = new HashMap<>();
+	String str = br.readLine();
+	StringTokenizer st = new StringTokenizer(str); 
+	for(int i =0;i<N;++i) {
+		String key = st.nextToken();
+		Long value = Long.parseLong(key);
+		map.put(key, value);
+	}
+	StringBuilder sb = new StringBuilder();
+	int M = Integer.parseInt(br.readLine());
+	String str2 = br.readLine();
+	StringTokenizer st2 = new StringTokenizer(str2);
+	for(int i =0;i<M;++i) {
+		String key = st2.nextToken();
+		Long value = Long.parseLong(key);
+		try {
+			if(map.get(key).equals(value)) {
+				sb.append(1).append("\n"); 
+			}
+		} catch (Exception e) {
+			sb.append(0).append("\n");
+		}
+	}
+	System.out.println(sb.toString());
+}
+}
