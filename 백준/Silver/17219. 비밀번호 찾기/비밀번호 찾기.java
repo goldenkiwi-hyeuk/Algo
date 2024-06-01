4a953e682c5976c6fe60d46ebcd8dbd5961cbb4e
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-06-01 11:07:25 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 624 ms, Memory: 55432 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,20 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String numbers = br.readLine();
+		StringTokenizer st1 = new StringTokenizer(numbers);
+		int N = Integer.parseInt(st1.nextToken());
+		int M = Integer.parseInt(st1.nextToken());
 		HashMap<String, String> map = new HashMap<>();
-		for(int i=0; i<N;++i) {
-			String site = sc.next();
-			String pw = sc.next();
+		for(int i = 0; i<N ;++i) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str);
+			String site = st.nextToken();
+			String pw = st.nextToken();
 			map.put(site, pw);
 		}
-		for(int i=0; i<M;++i) {
-			String site = sc.next();
-			System.out.println(map.get(site));
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i<M ;++i) {
+			String site = br.readLine();
+			sb.append(map.get(site)).append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 }
