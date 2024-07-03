88efaedb428cc63f56734003bf5da8773c1d83d0
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-07-03 13:38:15 +0900

    [Silver II] Title: 좌표 압축, Time: 3536 ms, Memory: 355148 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int[] arr = new int[N];
+        for(int i=0; i<N;++i){
+            int key = Integer.parseInt(st.nextToken());
+            arr[i] = key;
+            if(!map.containsKey(key)){
+                map.put(key,1);
+            }
+        }
+        Integer[] arr2 = map.keySet().toArray(new Integer[0]);
+        Arrays.sort(arr2);
+        for(int i=0; i<arr2.length;++i){
+            map.put(arr2[i],i);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int key : arr){
+            sb.append(map.get(key)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
