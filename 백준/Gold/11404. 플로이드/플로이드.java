6a0cfa07d80c0328a3921a8f1514924757ff25c9
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-11-25 16:49:52 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 44948 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N][N];
+        int INF = 987654321;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dp[i][j] = INF;
+            }
+        }
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            dp[start - 1][end - 1] = Math.min(dp[start - 1][end - 1], cost);
+        }
+
+        for (int s = 0; s < 2; s++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    for (int k = 0; k < N; k++) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                        dp[j][i] = Math.min(dp[j][i], dp[j][k] + dp[k][i]);
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    sb.append(0).append(" ");
+                } else if (dp[i][j] == INF) {
+                    sb.append(0).append(" ");
+                } else {
+                    sb.append(dp[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
