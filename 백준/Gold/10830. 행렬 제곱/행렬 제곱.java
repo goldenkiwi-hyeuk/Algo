a0a57a86e541bf4e2eae90371553ea00e952e387
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-12-04 19:36:36 +0900

    [Gold IV] Title: 행렬 제곱, Time: 72 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static long B;
+    static int mod = 1000;
+    static int[][] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        N = Integer.parseInt(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        A = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            str = br.readLine();
+            st = new StringTokenizer(str);
+            for (int j = 0; j < N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        if(B == 1){
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sb.append(A[i][j]%1000).append(" ");
+                }
+                sb.append("\n");
+            }
+        } else {
+            int[][] answer = getpow(B);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sb.append(answer[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }   
+        }
+        System.out.println(sb);
+    }
+
+    private static int[][] getpow(long b) {
+        if (b == 1) {
+            return A;
+        }
+        if (b % 2 == 0) {
+            int[][] temp = getpow(b / 2);
+            return multiply(temp, temp);
+        } else {
+            int[][] temp = getpow(b / 2);
+            return multiply(multiply(temp, temp), A);
+        }
+    }
+
+    private static int[][] multiply(int[][] temp1, int[][] temp2) {
+        int[][] answer = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int num = 0;
+                for (int k = 0; k < N; k++) {
+                    num = ((temp1[i][k] * temp2[k][j]) % mod + (num % mod)) % mod;
+                }
+                answer[i][j] = num;
+            }
+        }
+        return answer;
+    }
+}
