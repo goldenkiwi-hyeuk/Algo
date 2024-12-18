652c19f29a5e4338888a91eafbb66ca5a05596c4
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-12-18 15:55:47 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 564 ms, Memory: 61816 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static class Edge implements Comparable<Edge> {
+        int end, cost;
+
+        public Edge() {
+        }
+
+        public Edge(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<List<Edge>> nodeList = new ArrayList<>();
+        for (int i=0; i<=N; i++) {
+            nodeList.add(new ArrayList<Edge>());
+        }
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            nodeList.get(start).add(new Edge(end, cost));
+        }
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int startPoint = Integer.parseInt(st.nextToken());
+        int endPoint = Integer.parseInt(st.nextToken());
+        int[] dist = new int[N+1];
+        Arrays.fill(dist, 987654321);
+        dist[startPoint] = 0;
+        boolean[] visited = new boolean[N+1];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        List<List<Integer>> routes = new ArrayList<>();
+        for (int i=0; i<=N; i++) {
+            routes.add(new ArrayList<Integer>());
+        }
+        routes.get(startPoint).add(startPoint);
+        pq.add(new Edge(startPoint, 0));
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (visited[edge.end]) continue;
+            visited[edge.end] = true;
+            for (Edge e : nodeList.get(edge.end)) {
+                if (dist[e.end] > dist[edge.end] + e.cost) {
+                    dist[e.end] = dist[edge.end] + e.cost;
+                    routes.get(e.end).clear();
+                    routes.get(e.end).addAll(routes.get(edge.end));
+                    routes.get(e.end).add(e.end);
+                    pq.add(new Edge(e.end, dist[edge.end]+e.cost));
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(dist[endPoint]).append("\n");
+        sb.append(routes.get(endPoint).size()).append("\n");
+        for (Integer i : routes.get(endPoint)) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
