9053de7aede060654dce4d46a740cad341f39190
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2025-01-15 19:21:20 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 104 ms, Memory: 12276 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -20,14 +20,14 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < i; ++j) {
                 if (arr[i] > arr[j] && dp1[i] <= dp1[j]) {
-                    dp1[i] = dp1[i] + 1;
+                    dp1[i] = dp1[j] + 1;
                 }
             }
         }
         for (int i = N-1; i >= 0; i--) {
             for (int j = N-1; j > i; j--) {
                 if (arr[i] > arr[j] && dp2[i] <= dp2[j]) {
-                    dp2[i] = dp2[i] + 1;
+                    dp2[i] = dp2[j] + 1;
                 }
             }
         }
