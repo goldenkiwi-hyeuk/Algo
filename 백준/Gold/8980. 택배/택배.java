f325afc47ae8189478764d88bd804ca25f9a7d46
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2025-02-17 01:21:52 +0900

    [Gold II] Title: 택배, Time: 236 ms, Memory: 18660 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.java
+++ 백준/Gold/8980. 택배/택배.java
@@ -2,32 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
+
     // 기본 아이디어
-    // 택배라는 객체 생성이후 거리(택배의 도착점 - 택배의 출발점)계산후 거리가 짧은순으로 정렬 만약 거리가 같다면 출발점이 빠른 순으로 정렬
-    // 택배의 출발점부터 도착점-1까지 가지고 갈수 있는 용량을 구함 Math.min활용
+    // 택배라는 객체 생성이후 도착점 순서로 정렬, 만약 도착점이 동일하다면 출발점 순서로 정렬
     // 결과값을 더하기
-    
+
     private static class TackBae implements Comparable<TackBae>{
         int start, end, amount;
-        
+
         public TackBae(){}
-        
+
         public TackBae(int start, int end, int amount){
             this.start = start;
             this.end = end;
             this.amount = amount;
         }
-        
+
         @Override
         public int compareTo(TackBae o){
-            if((this.end-this.start) == (o.end-o.start)){
-                return this.start - o.start;    
+            if(this.end == o.end){
+                return o.start - this.start;
             }
-            return (this.end-this.start) - (o.end-o.start);
+            return this.end - o.end;
         }
     }
-    
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
@@ -48,18 +47,15 @@ public class Main {
         int ans = 0;
         while(!pq.isEmpty()){
             TackBae tackbae = pq.poll();
-            int amount = 987654321; // 최소값을 가져갈 예정
-            for(int i = tackbae.start; i< tackbae.end; ++i){
-                amount = Math.min(Math.min(amount, C-arr[i]),tackbae.amount); // 여태까지 계산된 최소값, 현재 위치에 탑차 가능한 물류량, 택배 자체 물류량 3개중 가장 작은 값 판별
-                if(amount == 0){ // 중간에 0이 나온다면 해당 택배는 배송불가
-                    break;
-                }
+            int amount = tackbae.amount;
+            for(int i = tackbae.start; i<tackbae.end ; ++i){
+                amount = Math.min(amount,C-arr[i]);
             }
-            if(amount != 0){
-                for(int i = tackbae.start; i< tackbae.end; ++i){ // arr[i] 최신화
-                    arr[i] += amount; // 계획 상 각 지점에서 가지고 있는택배량 더하기
+            if(amount>0){
+                for(int i = tackbae.start; i<tackbae.end ; ++i){
+                    arr[i]+= amount;
                 }
-                ans += amount; // 가능한 운반 택배 갯수 더해주기
+                ans += amount;
             }
         }
         System.out.println(ans);
