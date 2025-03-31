4deeafd6525a7dbc56f7a0b4974ca4c240ebb755
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2025-03-31 09:40:09 +0900

    [Gold II] Title: 반도체 설계, Time: 2048 ms, Memory: 19048 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    
+    // 기본 아이디어 : 2중 for문을 활용한 1차원 dp 
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -13,10 +16,10 @@ class Main {
         int[] dp = new int[N+1];
         
         for(int i = 1; i<=N;++i){
-            for(int j = arr[i]; j<=N;++j){
-                if(j == arr[i]){
-                    dp[j] = dp[j-1]+1;
-                } else {
+            for(int j = arr[i]; j<=N;++j){ // 연결가능한 지점부터 뒤로 돌면서
+                if(j == arr[i]){ // 연결 가능한 지점이라면 그 지점 전값 + 1
+                    dp[j] = dp[j-1]+1; 
+                } else { // 연결 가능한 지점의 뒤라면 앞과 현재 값중 큰 값 저장
                     dp[j] = Math.max(dp[j-1], dp[j]);
                 }
             }
