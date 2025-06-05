12f4dea2a59fbef3ba83efa4e7415f820ad29233
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2025-06-05 09:35:40 +0900

    [Gold II] Title: 할로윈의 양아치, Time: 856 ms, Memory: 567776 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
@@ -2,6 +2,10 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    
+    // 기본아이디어 : 유니온 파이드 + 배낭문제
+    // 먼저 유니온 파인드를 통해서 아이들을 그룹짓고 그룹의 인원수와 전체 사탕갯수를 체크한다.
+    // 그룹의 인원수를 배낭의 무게, 사탕의 갯수를 물건의 가치로 선정하여 냅색을 한다.
     static int N,M,K;
     static int[] candy, parent;
     public static void main(String[] args) throws IOException {
@@ -21,6 +25,7 @@ class Main {
             parent[i] = i;
         }
         
+        // 아이들 그룹짓기
         for(int i = 1; i<=M;++i){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -28,12 +33,15 @@ class Main {
             unionParent(a,b);
         }
         
+        // 캔디리스트의 각 행의 [0]에는 각 그룹의 인원수, [1]에는 각 그룹의 사탕갯수가 체크된다.
         int[][] candyList = new int[N+1][2];
         for(int i = 1; i<=N;++i){
             int parentNum = findParent(i);
             ++candyList[parentNum][0];
             candyList[parentNum][1] += candy[i];
         }
+        
+        // 그룹 인원수를 기준으로 오름차순 정렬, 인원수가 같다면 캔디를 기준으로 내림차순 정렬
         Arrays.sort(candyList, (o1,o2)->{
             if(o1[0]== o2[0]){
                 return o2[1]-o1[1];
@@ -42,15 +50,13 @@ class Main {
             }
         });
         
-        // for(int i = 0; i<=N; ++i){
-        //     System.out.println(Arrays.toString(candyList[i]));
-        // }
-        
         int[][] dp = new int [N+1][K];
         for(int i = 1; i<=N; ++i){
+            // 배열을 넉넉하게 만들었기에 그룹이 0명인 그룹은 체크하지 않고 넘어간다.
             if(candyList[i][0] == 0){
                 continue;
             }
+            // 기본적인 배낭문제
             for(int j = 1; j<K; ++j){
                 if(candyList[i][0]<=j){
                     dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-candyList[i][0]]+candyList[i][1]);
@@ -60,16 +66,11 @@ class Main {
             }
         }
         
-        // for(int i = 0; i<=N; ++i){
-        //     System.out.println(Arrays.toString(dp[i]));
-        // }
-        
         System.out.println(dp[N][K-1]);
     }
     
     private static int findParent(int num){
         if(parent[num]==num) return num;
-        
         return parent[num] = findParent(parent[num]);
     }
     
