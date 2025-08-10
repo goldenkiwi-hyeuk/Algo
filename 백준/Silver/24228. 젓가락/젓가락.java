9da1ed8dc84154bbba27463e96a612baf2a55f2b
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2025-08-10 22:34:13 +0900

    [Silver V] Title: 젓가락, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long N = Long.parseLong(st.nextToken());
+        long R = Long.parseLong(st.nextToken());
+        
+        System.out.println(N + 1 + 2 * (R - 1));
+    }
+}
