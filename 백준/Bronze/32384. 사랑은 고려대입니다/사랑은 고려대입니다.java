eff532a0bb5336dbcde1cc9736590bc9a17775cb
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2026-04-24 00:44:40 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 92 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int n = sc.nextInt();
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             sb.append("LoveisKoreaUniversity").append(" ");
         }
         System.out.println(sb.toString());
